4cac1fe9ddd97bd264ba39dae73bf61e25e2ffc1
athar <athar@THUGSHAKERZ>
2024-03-28 11:16:17 +0700

    menambah void input

--- QuickSort_0184/QuickSort_0184.cpp
+++ QuickSort_0184/QuickSort_0184.cpp
@@ -7,3 +7,26 @@ int cmp_count = 0; // number of comparisons
 int mov_count = 0; // number of data movements
 int n; 
 
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang elemen array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+
+    cout << "\n----------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "-----------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+
