24632c4c6d31f6ef96496d7fff3de5c9fa756760
athar <athar@THUGSHAKERZ>
2024-03-28 11:21:53 +0700

    menambah void display

--- QuickSort_0184/QuickSort_0184.cpp
+++ QuickSort_0184/QuickSort_0184.cpp
@@ -84,3 +84,17 @@ void q_short(int low, int high)
     q_short(j + 1, high); //step 13
 }
 
+void display() {
+    cout << "\n------------------" << endl;
+    cout << "Sorted array" << endl;
+    cout << "--------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparisons: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
+
