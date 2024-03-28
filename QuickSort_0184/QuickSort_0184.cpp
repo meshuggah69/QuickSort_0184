cf39d98480904e3fd0dbfa4387af515280f50128
athar <athar@THUGSHAKERZ>
2024-03-28 11:19:57 +0700

    menambah void q hhort

--- QuickSort_0184/QuickSort_0184.cpp
+++ QuickSort_0184/QuickSort_0184.cpp
@@ -39,3 +39,48 @@ void swaps(int x, int y)
     mov_count++;
 }
 
+void q_short(int low, int high)
+{
+    int temp;
+    int pivot, i, j;
+    if (low > high) { //step 1
+        return;
+    }
+
+    pivot = arr[low]; //step 2 
+    i = low + 1; //step 3
+    j = high; //step 4
+
+    while (i <= j) //step 10
+    {
+        //search for an element greater than pivot
+        while ((arr[i] <= pivot) && (i <= high)) //step 5
+        {
+            i++; //step 6
+            cmp_count++;
+        }
+        cmp_count++;
+        //search for an element less or equal to pivot
+        while ((arr[j] > pivot) && (j >= low)) //step 7
+        {
+            j--; //step 8
+            cmp_count++;
+        }
+        cmp_count++;
+        if (i < j) //step 9
+        {
+            //swap the element at index i with the element at index j
+            swaps(i, j);
+        }
+    }
+    if (low < j) //step 11
+        //swap the pivot element with the element at index j
+        swaps(low, j);
+
+    //recursive call to sort the left sub array
+    q_short(low, j - 1); //step 12
+
+    //recursive call to sort the right sub array
+    q_short(j + 1, high); //step 13
+}
+
