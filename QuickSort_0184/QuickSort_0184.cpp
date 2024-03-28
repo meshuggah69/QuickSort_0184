4b9a407c7e7354d3cdd9c3abf708a1275513cdf6
athar <athar@THUGSHAKERZ>
2024-03-28 11:15:23 +0700

    menambah int mov count

--- QuickSort_0184/QuickSort_0184.cpp
+++ QuickSort_0184/QuickSort_0184.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 //array of integers to hold values
 int arr[20];
-int cmp_count = 0; // number of comparisons
+int cmp_count = 0; // number of comparisons 
 int mov_count = 0; // number of data movements
 int n;
 
